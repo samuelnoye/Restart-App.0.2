6aa650893ed1d1ba752e802665a7c838a88eebd1
Samuel Noye <samuel.noye@amalitech.org>
2021-12-08 08:53:49 +0000

    Update OnboardingView.swift

--- Restart-App.0.2/Screens/OnboardingView.swift
+++ Restart-App.0.2/Screens/OnboardingView.swift
@@ -13,6 +13,11 @@ struct OnboardingView: View {
     
   
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat =
+    0
+    @State private var isAnimating: Bool =
+    false
     
     //MARK:- BODY
     
@@ -73,7 +78,7 @@ struct OnboardingView: View {
                     HStack{
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
                         Spacer()
                     }
                     
@@ -89,14 +94,29 @@ struct OnboardingView: View {
                             .font(.system(size: 24, weight:  .bold))
                     }.foregroundColor(.white)
                             .frame(width:80, height:80, alignment: .center)
-                            .onTapGesture {
-                            isOnboardingViewActive = false
-                            }
+                            .offset(x:buttonOffset )
+                            .gesture(
+                            DragGesture()
+                                .onChanged{ gesture in
+                                    if gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80{
+                                        buttonOffset = gesture.translation.width
+                                    }
+                                }
+                                .onEnded{ _ in
+                                    if buttonOffset > buttonWidth / 2{
+                                         buttonOffset = buttonWidth - 80
+                                        isOnboardingViewActive = false
+                                    }else{
+                                    buttonOffset = 0
+                                   
+                                    }
+                                }
+                            )//: Gesture
                         Spacer()
                     }
                     
                 }
-                    .frame(height: 80, alignment: .center)
+                .frame(width: buttonWidth, height: 80, alignment: .center)
                     .padding()
                     
             }//: VStack
