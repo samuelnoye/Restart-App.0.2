5f151898d26bc8fd28fedf87dfc82efee85aa10f
Samuel Noye <samuel.noye@amalitech.org>
2021-12-08 11:21:21 +0000

    Update OnboardingView.swift

--- Restart-App.0.2/Screens/OnboardingView.swift
+++ Restart-App.0.2/Screens/OnboardingView.swift
@@ -19,6 +19,7 @@ struct OnboardingView: View {
     @State private var isAnimating: Bool =
     false
     @State private var imageOffset: CGSize = .zero
+    @State private var indicatorOpacity : Double = 1.0
     
     //MARK:- BODY
     
@@ -71,11 +72,16 @@ struct OnboardingView: View {
                                     
                                     if abs(imageOffset.width) <= 150{
                                         imageOffset = gesture.translation
-                                        
+                                        withAnimation(.linear(duration: 0.2)){
+                                            indicatorOpacity = 0
+                                        }
                                     }
-                                    }.onEnded{ _ in
+                                    }
+                                .onEnded{ _ in
                                         imageOffset = .zero
-                                    
+                                    withAnimation(.linear(duration: 0.25)){
+                                        indicatorOpacity = 1
+                                    }
                                 }
                         )
                         .animation(.easeOut(duration: 1), value: imageOffset)
@@ -88,6 +94,7 @@ struct OnboardingView: View {
                     .offset(y: 20)
                     .opacity(isAnimating ? 1 : 0)
                     .animation(.easeOut(duration: 1).delay(2),  value: isAnimating)
+                    .opacity(indicatorOpacity)
                 ,alignment: .bottom)
                 Spacer()
                 
