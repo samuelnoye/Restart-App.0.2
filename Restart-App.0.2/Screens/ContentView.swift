5786701c6ab0fa047233cfe7138163e8e0c07692
Samuel Noye <samuel.noye@amalitech.org>
2021-12-08 18:08:22 +0000

    update

---
+++ Restart-App.0.2/Screens/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Samuel Noye on 06/12/2021.
+//
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    var body: some View {
+        ZStack{
+            if isOnboardingViewActive{
+                OnboardingView()
+            }else{
+                HomeView()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
